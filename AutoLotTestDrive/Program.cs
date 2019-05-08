726f5356afc25742f744487e9d6908c422ccac08
Rafael <rap1raf@gmail.com>
2019-05-08 22:08:30 +0300

    AddNewProject

---
+++ AutoLotTestDrive/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoLotDAL.EF;
+using AutoLotDAL.Models;
+using System.Data.Entity;
+
+namespace AutoLotTestDrive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database.SetInitializer(new MyDataInitialiizer());
+            Console.WriteLine("Fun with ADO.NET EF Code First \n");
+            using (var context = new AutoLotEntities())
+            {
+                foreach (Inventory inventory in context.Inventory)
+                {
+                    Console.WriteLine(inventory);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
